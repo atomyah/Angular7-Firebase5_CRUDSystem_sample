697b343b156338f7bf01d31bc2067ba106e3166f
atom <atom@yah.bz>
2018-11-19 18:52:05 +0900

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Include components for in which router service to be used
+import { AddStudentComponent } from './add-student/add-student.component';
+import { StudentsListComponent } from './students-list/students-list.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
 
+
+// Routes array define component along with the path name for url
+const routes: Routes = [
+  { path: '', redirectTo: '/register-student', pathMatch: 'full' },
+  { path: 'register-student', component: AddStudentComponent },
+  { path: 'view-students', component: StudentsListComponent },
+  { path: 'edit-student/:id', component: EditStudentComponent } 
+];
+
+// Import RouterModule and inject routes array in it and dont forget to export the RouterModule 
+// CommonModuleはいらないと思う。わからないけど。
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBxGaT2BZQ5BYHheoMIY1wRf-EdIr927-Y",
+    authDomain: "student-app-2fa7d.firebaseapp.com",
+    databaseURL: "https://student-app-2fa7d.firebaseio.com",
+    projectId: "student-app-2fa7d",
+    storageBucket: "student-app-2fa7d.appspot.com",
+    messagingSenderId: "232183681472"
+  }
 };
 
 /*
